3f4024ac5a569eed075469f174c58f2a51229767
andreaspaps <96740937+andreaspaps@users.noreply.github.com>
2024-03-12 05:00:33 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    var iframe = document.createElement('iframe');
+    
+    iframe.src = 'https://www.youtube.com/embed/y0Ul9oH9zlc?controls=0&rel=0';
+    iframe.title = 'YouTube video player';
+    iframe.frameBorder = '0';
+    iframe.allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share';
+    iframe.setAttribute('allowfullscreen', '');
+    iframe.classList.add('responsive-video'); // Ensure you have CSS for this class
+    
+    document.getElementById('youtube-placeholder').appendChild(iframe);
+});
