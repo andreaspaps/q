e6053c72d51eb29968a80281d39b5d49722d08d7
andreaspaps <96740937+andreaspaps@users.noreply.github.com>
2024-03-13 00:35:59 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var observer = new IntersectionObserver(function(entries, observer) {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          var iframe = document.createElement('iframe');
-          
-          iframe.src = 'https://www.youtube.com/embed/y0Ul9oH9zlc?controls=0&rel=0';
-          iframe.title = 'YouTube video player';
-          iframe.frameBorder = '0';
-          iframe.allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share';
-          iframe.setAttribute('allowfullscreen', '');
-          iframe.classList.add('responsive-video');
-          
-          document.getElementById('youtube-placeholder').appendChild(iframe);
-          
-          // Once the iframe is loaded, there's no need to observe the placeholder
-          observer.unobserve(entry.target);
-        }
-      });
-    }, {
-      rootMargin: "900px" // Load a bit before the element is in view
+    // Add click event listener to the play icon
+    document.querySelector('.play-icon').addEventListener('click', function() {
+        // Replace the placeholder with the YouTube video iframe
+        loadYouTubePlayer();
     });
-    
-    observer.observe(document.getElementById('youtube-placeholder'));
 });
+
+function loadYouTubePlayer() {
+    var placeholder = document.getElementById('youtube-placeholder');
+    var iframe = document.createElement('iframe');
+    iframe.src = 'https://www.youtube.com/embed/y0Ul9oH9zlc?autoplay=1&controls=0&modestbranding=1&rel=0&title=0';
+    iframe.title = 'YouTube video player';
+    iframe.frameBorder = '0';
+    iframe.allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture';
+    iframe.setAttribute('allowfullscreen', '');
+    iframe.classList.add('responsive-video');
+
+    // Remove the placeholder image and play icon
+    placeholder.innerHTML = '';
+    // Append the YouTube player iframe to the placeholder
+    placeholder.appendChild(iframe);
+}
