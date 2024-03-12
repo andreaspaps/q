27e98a3be144a3c693e402d951549e31329a6a26
andreaspaps <96740937+andreaspaps@users.noreply.github.com>
2024-03-12 08:21:08 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var iframe = document.createElement('iframe');
+    var observer = new IntersectionObserver(function(entries, observer) {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          var iframe = document.createElement('iframe');
+          
+          iframe.src = 'https://www.youtube.com/embed/y0Ul9oH9zlc?controls=0&rel=0';
+          iframe.title = 'YouTube video player';
+          iframe.frameBorder = '0';
+          iframe.allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share';
+          iframe.setAttribute('allowfullscreen', '');
+          iframe.classList.add('responsive-video');
+          
+          document.getElementById('youtube-placeholder').appendChild(iframe);
+          
+          // Once the iframe is loaded, there's no need to observe the placeholder
+          observer.unobserve(entry.target);
+        }
+      });
+    }, {
+      rootMargin: "900px" // Load a bit before the element is in view
+    });
     
-    iframe.src = 'https://www.youtube.com/embed/y0Ul9oH9zlc?controls=0&rel=0';
-    iframe.title = 'YouTube video player';
-    iframe.frameBorder = '0';
-    iframe.allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share';
-    iframe.setAttribute('allowfullscreen', '');
-    iframe.classList.add('responsive-video'); // Make sure you define the CSS for responsiveness
-    
-    document.getElementById('youtube-placeholder').appendChild(iframe);
-  });
+    observer.observe(document.getElementById('youtube-placeholder'));
+});
